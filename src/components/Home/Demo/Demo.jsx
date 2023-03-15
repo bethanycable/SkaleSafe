18b10cff579a3d8fd6c2688a69b20ebd1f3024bc
Bethany Mattern <bethany.a.cable@gmail.com>
2023-03-15 15:31:58 -0400

    Update Demo styling

--- src/components/Home/Demo/Demo.jsx
+++ src/components/Home/Demo/Demo.jsx
@@ -7,12 +7,12 @@ const Demo = () => {
       className="h-[24rem] ml:h-[42rem] w-screen flex flex-col items-center justify-center"
     >
       <h1
-        className="text-3xl text-honeydew font-bold ml:mt-6 mb-10 tracking-wider border-b-4"
+        className="text-3xl text-honeydew font-bold ml:mt-6 mb-6 ml:mb-10 tracking-wider border-b-4"
       >
         DEMO
       </h1>
       <iframe
-        className='w-5/6 h-3/5 ml:w-[750px] ml:h-[422px]'
+        className='w-5/6 h-2/3 ml:w-[750px] ml:h-[422px]'
         src="https://www.youtube.com/embed/8zvLTEEqlmg"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
