7688dada796e255428ba28f947a675cb9bb82d85
Bethany Mattern <bethany.a.cable@gmail.com>
2023-03-15 15:29:54 -0400

    Update demo video size

--- src/components/Home/Demo/Demo.jsx
+++ src/components/Home/Demo/Demo.jsx
@@ -4,17 +4,15 @@ const Demo = () => {
   return (
     <div
       id="demo"
-      className="h-[42rem] w-screen flex flex-col items-center justify-center"
+      className="h-[24rem] ml:h-[42rem] w-screen flex flex-col items-center justify-center"
     >
       <h1
-        id="demoText"
-        className="text-3xl text-honeydew font-bold mt-6 mb-10 tracking-wider border-b-4"
+        className="text-3xl text-honeydew font-bold ml:mt-6 mb-10 tracking-wider border-b-4"
       >
         DEMO
       </h1>
       <iframe
-        width="750"
-        height="422"
+        className='w-5/6 h-3/5 ml:w-[750px] ml:h-[422px]'
         src="https://www.youtube.com/embed/8zvLTEEqlmg"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
